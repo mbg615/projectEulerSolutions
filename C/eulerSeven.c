58b111f28769c23cccbad67fc249cc540f8d7340
mgb615 <m4dpup@gmail.com>
2021-03-08 22:03:54 -0600

    Cleaned up and simplified code

--- C/eulerSeven.c
+++ C/eulerSeven.c
@@ -1,28 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int testPrime(int number);
 int main(void) {
-    int counter = 6, numberToTest = 14, isPrime;
-    while(counter <= 10001) {
+    int counter = 0, numberToTest = 1, isPrime;
+    while(counter < 10001) {
+        numberToTest++;
         isPrime = testPrime(numberToTest);
         if(isPrime == 1) {
             counter++;
-            numberToTest++;
-        }
-        if(isPrime == 0) {
-            numberToTest++;
-        }
-        if(counter == 10001) {
-            numberToTest--;
-            printf("%d is the 10001st prime number\n", numberToTest);
-            exit(1);
         }
     }
+    printf("%d is the 10001st prime number\n", numberToTest);
 }
 int testPrime(int number) {
     for(int i = 2; i <= number/2; i++) {
-        if(number % i == 0) {
+        if(number % i == 0) q{
             return 0;
         }
     }
