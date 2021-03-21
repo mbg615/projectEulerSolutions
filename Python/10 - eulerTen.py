38122852fef30011d41bed0b0853701838049ef4
mgb615 <m4dpup@gmail.com>
2021-03-21 01:07:54 -0400

    Python sloution ten and structuring

---
+++ Python/10 - eulerTen.py
@@ -0,0 +1,14 @@
+import math
+
+def isPrime(n):
+    if n % 2 == 0 and n > 2: 
+        return False
+    return all(n % i for i in range(3, int(math.sqrt(n)) + 1, 2))
+
+if __name__ == "__main__":
+    sum = 0
+    for i in range(0, 2000000, 1):
+        if(isPrime(i)):
+            sum += i
+    print(sum)
+
