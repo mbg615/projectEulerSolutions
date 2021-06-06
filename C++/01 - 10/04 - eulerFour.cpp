491e7e15fc1273bb83914d0954c0a4edf5bdc475
mgb615 <m4dpup@gmail.com>
2021-06-06 17:40:18 -0500

    add more C++ solutions

---
+++ C++/01 - 10/04 - eulerFour.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main() {
+    int isPalindrome(int num);
+    list<int> palindromes;
+    for (int i = 999; i >= 100; i--) {
+        for (int j = 999; j >= 100; j--) {
+            int revNumber = 0;
+            revNumber = isPalindrome(i*j);
+            if (revNumber == i*j) {
+                palindromes.push_back(i*j);
+            }
+        }
+    }
+    palindromes.sort();
+    cout << palindromes.back() << "is the largest palindrome made from the products of 2 three digit numbers";
+    return 0;
+}
+
+int isPalindrome(int number) {
+    int digit, revNumber = 0;
+    while (number != 0) {
+        digit = number % 10;
+        revNumber = revNumber*10 + digit;
+        number /= 10;
+    }
+    return revNumber;
+}
