421362c00a3326a8062f954ae0b7843a7ffdaa1e
mbg615 <m4dpup@gmail.com>
2021-06-11 21:12:15 -0500

    cleanup and C++ solutions

---
+++ C++/01 - 10/05 - eulerFive.cpp
@@ -0,0 +1,15 @@
+ #include <iostream>
+
+using namespace std;
+
+int main() {
+    int dividend = 1;
+    bool running = true;
+    while (running == true) {
+        if (!(dividend % 11) && !(dividend % 12) && !(dividend % 13) && !(dividend % 14) && !(dividend % 15) && !(dividend % 16) && !(dividend % 17) && !(dividend % 18) && !(dividend % 19) && !(dividend % 20)) {
+                cout << dividend << " is the smallest number that is divisible by 1 through 20\n";
+                return 0;
+        }
+        dividend++;
+    }
+}
